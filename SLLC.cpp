29b64d11eb5929ccd11a279f2d0f18e72e9e783a
Embu Banjar <48594974+Embu48@users.noreply.github.com>
2019-04-03 20:18:41 +0700

    menu - 1

--- SLLC.cpp
+++ SLLC.cpp
@@ -3,3 +3,14 @@
 #include <cstring>
 #include <windows.h>
 using namespace std;
+
+void menu(){
+    char pilih1, pilih2, ulang;
+    int data,x;
+
+    do{
+      	system("cls");
+      	cout<<"SINGLE LINKED LIST CIRCULAR\n---------------------------\n\n";
+      	cetak();
+      	cout<<"\nMenu : \n1. Input data\n2. Hapus data\n3. Keluar\n>>";
+      	cin>>pilih1;
