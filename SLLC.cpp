b011b97e976269e77831bbe7f1ed37d8e11fc16f
Muhamad Sepryan Astrayesa <48191861+Astrayesa@users.noreply.github.com>
2019-04-03 20:53:13 +0700

    Update SLLC.cpp

--- SLLC.cpp
+++ SLLC.cpp
@@ -4,6 +4,58 @@
 #include <windows.h>
 using namespace std;
 
+struct TNode{
+	int data;
+	TNode *next;
+};
+
+TNode *head, *tail;
+
+void init(){
+	head = NULL;
+	tail = NULL;
+}
+
+int isEmpty(){
+	if(tail == NULL) 
+		return 1;
+	else 
+		return 0;
+}
+
+void hapusKhusus(int n){
+	TNode *bantu,*hapus;
+	int d,f=0;
+	bantu = head;
+		if (head->data == n){
+			hapus=head;
+			d=hapus->data;
+			head=head->next;
+			delete hapus;
+			cout<<endl<<d<<" terhapus\n";
+		}
+		else{
+			while((bantu->next)->data != n){
+				bantu=bantu->next;
+				if(bantu == tail){
+					f=1;
+					break;
+				}
+			}
+			if(f==1)
+				cout<<"\nTidak ada nilai "<<n<<" pada list\n";
+			else{
+				hapus=bantu->next;
+				d=hapus->data;
+				bantu->next=hapus->next;
+				if (hapus == tail)
+					tail=bantu;
+				delete hapus;
+				cout<<endl<<d<<" terhapus\n";
+			}
+		}
+}
+
 void menu(){
     char pilih1, pilih2, ulang;
     int data,x;
@@ -45,4 +97,29 @@ void menu(){
             		default :
             			cout<<"\nPilih ulang"<<endl;
 				}
+				break;
+
+      		case '2' :
+      			if (isEmpty() == 1){
+				  	cout<<"\n(List masih kosong)\n";
+      				break;
+      			}
+            		cout<<"\nHapus : \n1. Depan\n2. Belakang\n3. Khusus\n>>";
+            		cin>>pilih2;
+				
+            		switch(pilih2){
+      				case '1' :
+      					hapusDepan();
+            				break;
+            			case '2' :
+            				hapusBelakang();
+            				break;
+            			case '3' :
+            				cout<<"\nMasukkan nilai : ";
+            				cin>>data;
+            				hapusKhusus(data);
+            				break;
+            			default :
+            				cout<<"\nPilih ulang"<<endl;
+					}
 				break;
